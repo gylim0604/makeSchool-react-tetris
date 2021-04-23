45f06b948aefa08b004ed2de219477c974067286
gylim0604 <gylim0604@hotmail.com>
2021-04-23 15:59:25 +0800

    added next block section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 import GridBoard from './components/GridBoard/GridBoard';
+import NextBlock from './components/NextBlock/NextBlock';
 
 function App() {
   return(
@@ -9,6 +10,7 @@ function App() {
         <h1 className="App-title">Tetris Redux</h1>
       </header>
       <GridBoard/>
+      <NextBlock/>
     </div>
   );
 
---
+++ src/components/NextBlock/NextBlock.js
@@ -0,0 +1,18 @@
+import GridSquare from '../GridSquare/GridSquare';
+import './styles.css'
+export default function NextBlock(props){
+
+    const box = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+
+    const grid = box.map((rowArray,row)=>{
+        return rowArray.map((square,col)=>{
+            return <GridSquare key={`${row}${col}`} color={square}/>
+        })
+    })
+
+    return (
+        <div className="next-block">
+            {grid}
+        </div>
+    )
+}
