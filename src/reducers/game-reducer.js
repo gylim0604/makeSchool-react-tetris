b131c62a10d2ae1662f7f2b379dd193cec1cc470
gylim0604 <gylim0604@hotmail.com>
2021-04-24 12:19:28 +0800

    working on reducers

---
+++ src/reducers/game-reducer.js
@@ -0,0 +1,11 @@
+import {
+    MOVE_RIGHT, MOVE_LEFT, MOVE_DOWN, ROTATE,
+    PAUSE, RESUME, RESTART, GAME_OVER
+} from '.../actions';
+
+const gameReducer = (state = {}, action) =>{
+    switch(action.type){
+        case ROTATE:
+            break;
+    }
+}
