aebaf4316d6f4e7dfd6ec2c1799d5c4c7aa3961e
gylim0604 <gylim0604@hotmail.com>
2021-04-24 21:50:16 +0800

    added reducer skeleton

--- src/reducers/game-reducer.js
+++ src/reducers/game-reducer.js
@@ -1,11 +1,44 @@
 import {
-    MOVE_RIGHT, MOVE_LEFT, MOVE_DOWN, ROTATE,
-    PAUSE, RESUME, RESTART, GAME_OVER
+  MOVE_RIGHT,
+  MOVE_LEFT,
+  MOVE_DOWN,
+  ROTATE,
+  PAUSE,
+  RESUME,
+  RESTART,
+  GAME_OVER,
 } from '.../actions';
 
-const gameReducer = (state = {}, action) =>{
-    switch(action.type){
-        case ROTATE:
-            break;
-    }
-}
+const gameReducer = (state = {}, action) => {
+  switch (action.type) {
+    case ROTATE:
+      return state;
+
+    case MOVE_RIGHT:
+      return state;
+
+    case MOVE_LEFT:
+      return state;
+
+    case MOVE_DOWN:
+      return state;
+
+    case RESUME:
+      return state;
+
+    case PAUSE:
+      return state;
+
+    case GAME_OVER:
+      return state;
+
+    case RESTART:
+      return state;
+
+    default:
+      return state;
+  }
+};
+
+
+export default gameReducer;
